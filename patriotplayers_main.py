4f0b3688fffc3e17307608261c05dfd143a6112d
tuckersnow1 <97307919+tuckersnow1@users.noreply.github.com>
2023-09-04 09:53:46 -0400

    Update patriotplayers_main.py

--- patriotplayers_main.py
+++ patriotplayers_main.py
@@ -1,1 +1,16 @@
+import streamlit as st
 
+def main():
+    st.title("PatriotPlayers")
+
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+
+    if st.button("Login"):
+        if username == "admin" and password == "admin":
+            st.success("Logged in successfully")
+        else:
+            st.error("Invalid credentials")
+
+if __name__ == "__main__":
+    main()
