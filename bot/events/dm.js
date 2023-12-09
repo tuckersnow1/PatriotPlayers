00cc6230971d55c0bff9db2ef51b25c45f89915c
Anup Sinkar <132852593+aasink@users.noreply.github.com>
2023-12-09 11:35:40 -0500

    discord bot files

--- bot/events/dm.js
+++ bot/events/dm.js
@@ -1,9 +1,31 @@
+const { server_id } = require('../botconfig.json');
+
 module.exports = {
     name: 'dm',             // send a the msg to the user with userid
-    async execute(client, msg, userid) {
+    async execute(client, msg, username) {
       try{
-        const user = await client.users.fetch(userid);
-        user.send(msg);
+        //const user = await client.users.fetch(userid);      // expects userid instead of username
+        //user.send(msg);
+
+        const guild = await client.guilds.cache.get(server_id);
+        const members = await guild.members.fetch();
+
+        lcreator = members.find((member) => {
+          if(member.user.username === username) {
+            return true;
+          }
+          else if(member.user.username + '#' + member.user.discriminator === username) {
+            return true;
+          }
+          else if(member.user.globalName === username) {
+            return true;
+          }
+          else if(member.user.tag === username) {
+            return true;
+          }
+        });
+
+        lcreator.user.send(msg);
       }
       catch(err){
         console.log(err);
