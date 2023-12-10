5d42e68eba808c728000cb41dc8a13db9dd6a076
tuckersnow1 <97307919+tuckersnow1@users.noreply.github.com>
2023-12-09 20:10:29 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,68 +1,21 @@
-const http = require('http');
-const app = require('./app');
-// const client = require('./bot.js'); 
-// const test = require('./bottests.js'); 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CreateJoinPage from './patriot-players/src/CreateJoinPage';
+import CreatePage from './patriot-players/src/CreatePage';
 
-const normalizePort = val => {
-  const port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    return val;
-  }
-  if (port >= 0) {
-    return port;
-  }
-  return false;
-};
-const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
-
-const errorHandler = error => {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges.');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use.');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-};
-
-const server = http.createServer(app);
-
-server.on('error', errorHandler);
-server.on('listening', () => {
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-  console.log('Listening on ' + bind);
-});
-
-server.listen(port);
-
-// //discord bot event listeners
-// client.on('ready', () => {
-//   client.events.get('ready').execute(client);
-// });
-
-// client.on('messageCreate', message => {
-//   client.events.get('message').execute(client, message);
-// });
-
-// client.on('sendDm', (msg_tosend, userid) => {
-//   client.events.get('dm').execute(client, msg_tosend, userid);
-// });
-
-// client.on('channel', (channel_name, channel_type, action) => {
-//   client.events.get('channel').execute(client, channel_name, channel_type, action);
-// });
-
-// // test()
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/home" element={<CreateJoinPage />} />
+        <Route path="/create" element={<CreatePage />} />
+        {/* Add more routes as needed */}
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
